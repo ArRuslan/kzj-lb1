c0519d535ea7bec864105dfc57ba74f442a45411
RuslanUC <dev_ruslan_uc@protonmail.com>
2025-09-27 14:40:51 +0300

    add calculations

--- app/src/main/java/ua/nure/kz/CalcServlet.java
+++ app/src/main/java/ua/nure/kz/CalcServlet.java
@@ -1,15 +1,22 @@
 package ua.nure.kz;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 
 @WebServlet("/test")
 public class CalcServlet extends HttpServlet {
+    private static final Log log = LogFactory.getLog(CalcServlet.class);
 
     public void init(ServletConfig config) {
         System.out.println("init btw");
@@ -19,15 +26,44 @@ public class CalcServlet extends HttpServlet {
         System.out.println("destroy btw");
     }
 
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String op = req.getParameter("op");
-        String a = req.getParameter("a");
-        String b = req.getParameter("b");
-        resp.getWriter().write("hfs it works yay\n");
-        resp.getWriter().write(String.valueOf(op) + "\n");
-        resp.getWriter().write(String.valueOf(a) + "\n");
-        resp.getWriter().write(String.valueOf(b) + "\n");
-        
-    }
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        log.debug("Processing request with doGet...");
 
+        String op = req.getParameter("op");
+        String aStr = req.getParameter("a");
+        String bStr = req.getParameter("b");
+
+        log.trace("op="+op);
+        log.trace("a="+aStr);
+        log.trace("b="+bStr);
+
+        int a, b;
+
+        try {
+            a = Integer.parseInt(aStr);
+            b = Integer.parseInt(bStr);
+        } catch(NumberFormatException e) {
+            log.error("Failed to parse numbers: {0}", e);
+            return;
+        }
+
+        int result;
+        switch (op) {
+            case "add" -> result = Calc.add(a, b);
+            case "subtract" -> result = Calc.subtract(a, b);
+            case "multiply" -> result = Calc.multiply(a, b);
+            case "divide" -> result = Calc.divide(a, b);
+            default -> {
+                log.error("Invalid operation: " + op);
+                return;
+            }
+        }
+
+        req.setAttribute("op", op);
+        req.setAttribute("a", a);
+        req.setAttribute("b", b);
+        req.setAttribute("result", result);
+
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
 }
--- app/src/main/webapp/index.jsp
+++ app/src/main/webapp/index.jsp
@@ -1,19 +1,49 @@
+<%@ page contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8" %>
+
 <html>
 <head>
-  <style>
-  </style>
+    <style>
+        .d-flex {
+            display: flex;
+        }
+
+        .row {
+            flex-direction: row;
+        }
+
+        .col {
+            flex-direction: column;
+        }
+
+        .gap-2 {
+            gap: 1.5rem;
+        }
+    </style>
 </head>
 <body>
-    <h2>Yo this is java calculator</h2>
-    <form method="GET" action="test">
-        <select name="op">
-            <option value="add">Add</option>
-            <option value="subtract">Subtract</option>
-            <option value="multiply">Multiply</option>
-            <option value="divide">Divide</option>
-        </select>
+<h2>Yo this is java calculator</h2>
+<form method="GET" action="test" class="d-flex col gap-2">
+    <input name="a" placeholder="A" type="number" value="${a}"/>
 
-        <button type="submit">Calculate this bih</button>
-    </form>
+    <select name="op">
+        <option value="add" <%= request.getAttribute("op").equals("add") ? "selected" : "" %> >Add</option>
+        <option value="subtract" <%= request.getAttribute("op").equals("subtract") ? "selected" : "" %> >Subtract</option>
+        <option value="multiply" <%= request.getAttribute("op").equals("multiply") ? "selected" : "" %> >Multiply</option>
+        <option value="divide" <%= request.getAttribute("op").equals("divide") ? "selected" : "" %> >Divide</option>
+    </select>
+
+    <input name="b" placeholder="B" type="number" value="${b}"/>
+
+    <button type="submit">Calculate this bih</button>
+</form>
+
+<%
+    Object result = request.getAttribute("result");
+    if (result != null) {
+%>
+<h3>Result: ${result}</h3>
+<% } %>
+
 </body>
 </html>
